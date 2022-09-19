3495ee12a91199cddcc7c482ad978b4a143e275a
Dawson M <107786093+mcthaydt@users.noreply.github.com>
2022-09-19 17:07:40 -0500

    Version 1.1 - The Real Launch Version

--- src/routes/Blog.svelte
+++ src/routes/Blog.svelte
@@ -5,13 +5,13 @@
 <div class="container">
 
 	<div style="width: 300px; height: auto; color: green; outline-style: ridge; border-radius: 3px; margin-top: 3rem; padding: 1rem;">
-		<typewriter>
+		<Typewriter interval={10}>
 			<h2>McThayDT’s Branding Guide for Game Developers</h2>
 			<h3>“A brand is a result. It’s a customer’s gut feeling about a product, service, or company.” - Marty Neumeier</h3>
+		</Typewriter>
 			<div style="display: flex; align-items: center; justify-content: space-between; padding-top: 5px;"> 
 				<a href="https://medium.com/@mcthaydt/mcthaydts-branding-guide-for-game-developers-5559371f3370">Read Post</a>
 			</div>
-		</typewriter>
 	</div>
 </div>
 
--- src/routes/Work.svelte
+++ src/routes/Work.svelte
@@ -5,9 +5,13 @@
 
 <div class="container">
 	<div style="width: 300px; height: auto; color: green; outline-style: ridge; border-radius: 3px; margin-top: 3rem; padding: 1rem; display: block;">
-		<h2>Cabaret Ball</h2>
+		<Typewriter interval={15}>
+			<h2>Cabaret Ball</h2>
+		</Typewriter>
 		<img src="https://user-images.githubusercontent.com/107786093/178137514-ac57850b-3306-45ca-ad08-a4827fe15f57.gif" alt="funny GIF" width="90%" height="75%">
+		<Typewriter interval={15}>
 		<h3>Precision-based 3D platformer controller for Godot Engine 4 Alpha Version 10 </h3> 
+		</Typewriter >
 		<div style="display: flex; align-items: center; justify-content: space-between; padding: 5px;"> 
 			<a href="https://github.com/mcthaydt/CabaretBallSprint1_Prototype">Live Project</a>
 			<a href="https://github.com/mcthaydt/CabaretBallSprint1_Prototype" style="margin-left: auto; padding-right: 1rem;">Source Code</a>
